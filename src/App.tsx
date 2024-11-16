2366aa9fc0c08bf0c100f79bcd48ccf439f221ea
Serkan Bayram <serkanbay02@hotmail.com>
2024-11-16 05:42:45 +0100

    Add color palette and render words on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,54 @@
 import "./index.css";
 
 export default function App() {
-  return <div className="text-red-300">Hello world</div>;
+  const words = [
+    "merhaba",
+    "elma",
+    "kitap",
+    "kalem",
+    "dostluk",
+    "sevgi",
+    "deniz",
+    "güneş",
+    "ay",
+    "yıldız",
+    "çay",
+    "kahve",
+    "aile",
+    "okul",
+    "bahçe",
+    "sokak",
+    "şehir",
+    "dağ",
+    "ormam",
+    "göl",
+    "kuş",
+    "göz",
+    "mutluluk",
+    "huzur",
+    "rüya",
+    "çocuk",
+    "ev",
+    "masa",
+    "sandık",
+    "zaman",
+  ];
+
+  return (
+    <div className="w-full min-h-[100dvh] bg-background flex items-center justify-center">
+      <div className="w-[900px] mb-24 h-[150px] flex flex-wrap overflow-y-hidden gap-5 border text-3xl font-custom-noto  font-semibold">
+        {words.map((word, index) => {
+          const letters = word.split("");
+
+          return (
+            <div className="text-black/30 flex" key={index}>
+              {letters.map((letter, index) => (
+                <div className={index === 0 ? "capitalize" : ""}>{letter}</div>
+              ))}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#100b0e",
+        background: "#fbfafb",
+        primary: "#986b82",
+        secondary: "#bfc9b2",
+        accent: "#94b597",
+      },
+
+      fontFamily: {
+        "custom-noto": ['"Noto Sans Mono"'],
+      },
+    },
   },
   plugins: [],
 };
