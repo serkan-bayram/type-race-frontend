cad993dceef7c3b849fa2e3a54faec6e805a44a8
Debian <Serkan Bayram>
2024-11-19 21:14:38 +0000

    Production changes

--- src/socket.ts
+++ src/socket.ts
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production" ? undefined : "http://localhost:4001";
+  process.env.NODE_ENV === "production" ? "https://typerace.serkanbayram.dev" : "http://localhost:4001";
 
 export const socket = io(URL, { autoConnect: false });
