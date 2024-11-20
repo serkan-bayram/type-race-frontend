f78bb1b7829cd6db51638e3743731690f6596454
Serkan Bayram <serkanbay02@hotmail.com>
2024-11-20 15:16:46 +0100

    Add date to scoreboard

--- src/routes/Scoreboard.tsx
+++ src/routes/Scoreboard.tsx
@@ -22,18 +22,25 @@ export default function Scoreboard() {
   return (
     <div className="font-custom-noto py-24 w-full min-h-[100dvh] bg-[#0C0C0C] flex flex-col items-center justify-center gap-8">
       <h1 className="text-lg font-semibold text-white">Scoreboard</h1>
-      <div className="flex flex-col w-1/4 gap-4 p-4 text-white rounded-lg bg-white/10">
+      <div className="flex flex-col w-full gap-4 p-4 text-white rounded-lg sm:w-2/3 bg-white/10">
         <div className="flex items-center justify-between gap-4 p-1 px-4 font-semibold text-white">
-          <div>Username</div>
-          <div>Score</div>
+          <div className="flex-1">Username</div>
+          <div className="flex-1">Score</div>
+          <div className="flex-1">Date</div>
         </div>
         {scores.map((score) => (
           <div
             key={score.id}
             className="flex items-center justify-between gap-4 p-1 px-4 rounded-lg even:bg-black/10"
           >
-            <div>{score.userName}</div>
-            <div>{score.score}</div>
+            <div className="flex-1">{score.userName}</div>
+            <div className="flex-1">{score.score} WPM</div>
+            <div className="flex-1">
+              {new Date(score.createdAt).toLocaleDateString(undefined, {
+                hour: "numeric",
+                minute: "numeric",
+              })}
+            </div>
           </div>
         ))}
       </div>
