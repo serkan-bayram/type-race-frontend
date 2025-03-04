70a878db7327329470ba00e436621ccfeb933600
Serkan <serkanbay02@hotmail.com>
2025-03-04 13:22:06 +0300

    Add .env file and read it from socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -2,8 +2,7 @@ import { io } from "socket.io-client";
 
 // "undefined" means the URL will be computed from the `window.location` object
 const URL =
-  process.env.NODE_ENV === "production"
-    ? "https://typerace.serkanbayram.dev"
-    : "http://localhost:4001";
+  import.meta.env.VITE_API_URL.split("/api")[0] ??
+  console.error("No socket url.");
 
 export const socket = io(URL, { autoConnect: false });
